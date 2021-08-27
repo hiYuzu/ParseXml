6236f4b19b88d09ad7b7dbe5d6eafd59d786b994
hiYuzu <pomelo.yuzu1997@gmail.com>
2021-08-27 15:20:45 +0800

    modify config

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -9,7 +9,7 @@
 //------------------------------------------------------------------------------
 
 
-namespace parseXml.Properties
+namespace ParseXml.Properties
 {
     /// <summary>
     ///   强类型资源类，用于查找本地化字符串等。
@@ -43,7 +43,7 @@ namespace parseXml.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("parseXml.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ParseXml.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
